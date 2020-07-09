e90c23447e47799badfe880cdde3e128e56c4cba
Shinji Chiba <35828374+shinjich@users.noreply.github.com>
2020-07-09 17:26:56 +0900

    Add files via upload

---
+++ AlignmentSample/AKDKCamLib.h
@@ -0,0 +1,4 @@
+﻿extern int AKDKCamCreate( k4a_device_t pK, int iWidthMax, int iHeightMax );
+extern int AKDKCamSetRGB( unsigned char* pRGB24, int iWidth, int iHeight );
+extern int AKDKCamSetBGR( unsigned char* pBGR24, int iWidth, int iHeight );
+extern void AKDKCamDestroy();
